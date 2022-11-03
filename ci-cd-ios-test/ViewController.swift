ecdb2e4b5a8fe78c88f78b10584668ac23883d62
Usama Fouad <usamaeltmsah@gmail.com>
2022-11-03 13:30:36 +0200

    first commit

---
+++ ci-cd-ios-test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ci-cd-ios-test
+//
+//  Created by Usama Fouad on 03/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    func sum(a: Double, b: Double) -> Double {
+        return a + b
+    }
+}
+
