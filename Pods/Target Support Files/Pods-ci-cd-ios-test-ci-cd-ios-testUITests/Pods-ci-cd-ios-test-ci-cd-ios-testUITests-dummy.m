13fd3775d81b686e440c0f4721ff63e8f3b463d4
usamaeltmsah <usamaeltmsah@gmail.com>
2023-12-24 14:49:26 +0200

    remove pods from project

--- Pods/Target Support Files/Pods-ci-cd-ios-test-ci-cd-ios-testUITests/Pods-ci-cd-ios-test-ci-cd-ios-testUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ci_cd_ios_test_ci_cd_ios_testUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ci_cd_ios_test_ci_cd_ios_testUITests
-@end
--- Pods/Target Support Files/Pods-ci-cd-ios-testTests/Pods-ci-cd-ios-testTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ci_cd_ios_testTests : NSObject
-@end
-@implementation PodsDummy_Pods_ci_cd_ios_testTests
-@end
